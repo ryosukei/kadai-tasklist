64e2a12d18892b826932985577ceffaff2f00f4a
Ryosuke Aono <ryosuke_aono@r.recruit.co.jp>
2021-07-16 16:21:14 +0900

    設定した

---
+++ src/main/java/models/Task.java
@@ -0,0 +1,17 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+}
